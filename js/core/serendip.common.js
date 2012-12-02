5830224fc08fe5b33f817e99f692a4960eca8726
Mats Mortensen <matsmortensen@gmail.com>
2012-12-02 11:51:29 +0100

    Fixed global variable

--- js/core/serendip.common.js
+++ js/core/serendip.common.js
@@ -1,42 +1,23 @@
-﻿/* General functions */
-if (!Array.prototype.filter) {
-  Array.prototype.filter = function(fun /*, thisp*/) {
-    var len = this.length >>> 0;
-    if (typeof fun != "function")
-    throw new TypeError();
-
-    var res = [];
-    var thisp = arguments[1];
-    for (var i = 0; i < len; i++) {
-      if (i in this) {
-        var val = this[i]; // in case fun mutates this
-        if (fun.call(thisp, val, i, this))
-        res.push(val);
-      }
-    }
-    return res;
-  };
-}
-
-
-/* Start Serendip library */
+﻿/* Start Serendip library */
 var Serendip = function () { };
 
 Serendip.Events = (function () {
     var my = {};
     
+    var _callbacks = {};
+    
     my.on = function(event, callback, context) {
-      var callbacks = this._callbacks || (this._callbacks = {});
+      var callbacks = _callbacks || (_callbacks = {});
       var events = callbacks[event] || (callbacks[event] = []);
       events.push({ callback: callback, context: context });
     };
 
     my.off = function(event, callback, context) {
       if (!callback && !context) {
-        delete this._callbacks[event];
+        delete _callbacks[event];
       }
 
-      var events = this._callbacks[event] || [];
+      var events = _callbacks[event] || [];
       for (var i = 0; i < events.length; i++) {
         if (!(callback && events[i].callback !== callback || context && events[i].context !== context)) {
           events.splice(i, 1);
@@ -46,11 +27,11 @@ Serendip.Events = (function () {
 
     my.trigger = function(event) {
       var args = Array.prototype.slice.call(arguments, 1);
-      var callbacks = this._callbacks || {};
+      var callbacks = _callbacks || {};
       var events = callbacks[event] || [];
       for (var i = 0; i < events.length; i++) {
         var evt = events[i];
-        evt.callback.apply(evt.context || this, args);
+        evt.callback.apply(evt.context || my, args);
       }
     };
     
